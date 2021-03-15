a14c5d35543d0499c34f56b02262e5a74829244d
Anastasia_Poleshuk <71152591+Lai-LMU@users.noreply.github.com>
2021-03-15 15:29:33 +0300

    feat: create a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length == undefined || str.length == 0){
+    return false;
+  }
+
+  let stack = [];
+
+  for (let i=0; i< str.length; i++){
+    if (GetType(str[i])== 'inviled'){
+      return false;
+    }
+    if (GetType(str[i])== 'open'){
+      stack.push(str[i]);
+    }
+    if (GetType(str[i])== 'close'){
+      if(stack.length>0 ){
+        let l = stack.pop();
+        if(str[i] != findClosed(l)){
+          return false;
+        }
+      } else{
+        return false;
+      }  
+    }
+    if (GetType(str[i])== 'openClose'){
+      if(stack.length>0){
+        if(str[i] == findClosed(stack[stack.length-1])){
+          stack.pop();
+        } else{
+          stack.push(str[i]);
+        }
+      } else{
+        stack.push(str[i]);
+      }  
+    }
+  }
+
+  if(stack.length == 0){
+    return true;
+  } else{
+    return false;
+  }
+
+
+  function GetType(c){
+    for (let j=0; j<bracketsConfig.length; j++){
+      if(bracketsConfig[j][0] == c && bracketsConfig[j][1] != c){
+        return 'open';  
+      } else if(bracketsConfig[j][0] == c && bracketsConfig[j][1] ==c){
+        return 'openClose';
+      }else if(bracketsConfig[j][1]  == c && bracketsConfig[j][0] != c){
+        return 'close';
+       } else if (c == undefined || c == null){
+        return 'inviled';
+      }
+    }
+  }
+
+  function findClosed(c){
+    for (let j=0; j<bracketsConfig.length; j++){
+      if(bracketsConfig[j][0] == c){
+        return bracketsConfig[j][1];
+      }
+    }
+    return false;
+  }
 }
